bd8a991a62fe719c188e3c0634ebd2997769e29b
Fabricio <fabrix.enriquez@gmail.com>
2022-09-13 16:44:06 -0300

    desafio-contador

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="App-header">
-      <ItemListContainer greeting='¡Hola!'/>
+      <ItemListContainer />
         <img src={logo} className="App-logo" alt="logo" />
         
       </header>
--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,9 @@
-const ItemListContainer = ({ greeting }) => {
+import ItemCount from "../ItemCount/ItemCount";
+
+
+const ItemListContainer = () => {
     return (
-      <div>{greeting}</div>
+      <ItemCount stock={10}/>
     )
   }
   
