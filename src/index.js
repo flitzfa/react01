f339a325c0b0bc36de8cbb6e8b7c3369ed3101c1
Fabricio <fabrix.enriquez@gmail.com>
2022-10-11 20:36:02 -0300

    desafio-firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkTgk6a-KFAYy0l6LeNxLZm02WMaUnDm8",
+  authDomain: "workshopflitzfa.firebaseapp.com",
+  projectId: "workshopflitzfa",
+  storageBucket: "workshopflitzfa.appspot.com",
+  messagingSenderId: "341845886435",
+  appId: "1:341845886435:web:15eaa2dbfcc7053393ccd6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
